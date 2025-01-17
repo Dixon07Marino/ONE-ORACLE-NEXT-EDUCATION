d527d06f4096573d898b3cc2ce63359ceae616d9
Dixon Marino <dmusicmsoccer14@gmail.com>
2025-01-17 14:56:17 -0500

    Curso de lógica de Programación con JS Completado - ONE-Oracle-Next-Education

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,30 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroLimite = parseInt(prompt("Vamos a jugar!\nHasta que número quieres adivinar?: "))
+let secretNumber = Math.floor(Math.random() * numeroLimite) + 1;
+let userNumber;
+let intentos = 1
+let limite = 3
 
-console.log(numeroUsuario);
+while (userNumber != secretNumber) {
+    userNumber = parseInt(prompt(`Coloca un número del 1 al ${numeroLimite}: `));
+    console.log(typeof(userNumber))
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    if (userNumber == secretNumber) {
+        alert(`Adivinaste el resultado, era: ${secretNumber}, que crack! Lo lograste ${intentos} ${intentos == 1 ? "vez!" : "veces!"}`) //operador ternario
+    }
+    else {
+        if (userNumber > secretNumber) {
+            alert("El numero secreto es menor!")
+        }
+        else {
+            alert("El numero secreto es mayor!")
+        }
+        intentos++
+
+        if (intentos > limite) {
+            alert(`El número maximo de intentos era: ${limite}`)
+            break;
+        }
+    }
 }
+
+alert("El juego acabó!!!")
---
+++ 2034-logica-programacion-1-Aula1/ejercicios/program.js
@@ -0,0 +1,16 @@
+alert("¡Bienvenida y bienvenido a nuestro sitio web!")
+let nombre = prompt("Hola, coloca tu nombre: ")
+let edad = prompt(nombre, "Coloca tu edad: ")
+let numeroDeVentas = 50
+let saldoDisponible = 1000
+let mensajeDeError = alert("¡Error! Completa todos los campos")
+alert(mensajeDeError)
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
+else {
+    alert("Eres menor de edad!")
+}
+
+
